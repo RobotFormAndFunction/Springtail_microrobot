12e264d12ace58e52dd6212b2afd888dcf6acae3
RobotFormAndFunction <98366383+RobotFormAndFunction@users.noreply.github.com>
2023-08-18 17:11:37 -0400

    Update comments

--- Software/src/main.cpp
+++ Software/src/main.cpp
@@ -5,8 +5,8 @@
 int main() {
 	DDRB = (1<<DDB0) | (1<<DDB1) ;          // set PB0 & PB1 as output
 	
-	ADMUX = (1<<MUX1) | (0<<MUX0);						//Select ADC2
-	ADCSRA = (1<<ADEN) | (ADPS2);			//Set ADC2 analog pin on PB2
+	ADMUX = (1<<MUX1) | (0<<MUX0);		//Select ADC2
+	ADCSRA = (1<<ADEN) | (ADPS2);		//Set ADC2 analog pin on PB2
 	
 	
 	while (1) {
@@ -15,16 +15,16 @@ int main() {
 		ADCSRA = ADCSRA | (1<<ADSC);
 		while (ADCSRA & (1<<ADSC));
 		
-		val = ADCL;
+		val = ADCL; //store the value from the ADC
 		
 		if (val>70){
 			
 			PORTB |= (1<<PORTB0);   // drive PD1 high
 			PORTB &= ~(1<<PORTB1);
-			_delay_ms(12.5);         // delay 100 ms
+			_delay_ms(12.5);         // delay 12.5 ms
 			PORTB |= (1<<PORTB1);
 			PORTB &= ~(1<<PORTB0);  // drive PD1 low
-			_delay_ms(12.5);         // delay 900 ms
+			_delay_ms(12.5);         // delay 12.5 ms
 			
 		}			
 			
